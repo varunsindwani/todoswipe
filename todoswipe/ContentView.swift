6d7a9a68fba27737e1c96f9861b97ed06b95d8c4
Varun Sindwani <varun@sindwani.com>
2020-06-21 19:23:01 -0400

    init

---
+++ todoswipe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todoswipe
+//
+//  Created by Varun Sindwani on 2020-06-21.
+//  Copyright © 2020 Varun Sindwani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
